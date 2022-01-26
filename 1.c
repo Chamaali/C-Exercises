268ea3990908d45de0059dbdbbd7c8c21f7d169a
Chamaali Dilka <chamalidilkapmnt1998@gmail.com>
2022-01-27 03:07:05 +0530

    Completed exercise

---
+++ 1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    int arr[] = {34, 56, 54, 32, 67, 89, 90, 32, 21};
+
+    int i;
+
+    for (i=8; i>=0; i--){
+        printf("%d\n", arr[i]);
+        
+    }
+
+    return 0;
+}
