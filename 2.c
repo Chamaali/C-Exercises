f9ead82c73f67708c58bea9642dca9b58f6fd6c9
Chamaali Dilka <chamalidilkapmnt1998@gmail.com>
2022-01-27 03:47:12 +0530

    completed exercise 2

---
+++ 2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+
+    int arr[10];
+    printf("Enter Your Number: ");
+    scanf("%d", arr);
+
+    
+
+
+    int i;
+    int j;
+
+    if(arr[i]==arr[j]){
+        printf("Yes");
+    }
+
+    else{
+        printf("No");
+    }
+   
+
+    
+    
+    
+    
+        
+    
+
+    return 0;
+}
